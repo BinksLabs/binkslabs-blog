d217fc9107d82accf0df70120a2275aa0c1ed55e
binkslabs <pyrotundra@icloud.com>
2022-08-03 21:20:13 -0400

    Add Google site verification meta tag to the headMetadata.js component.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         "DEV_API_URL": "http://localhost:4000",
-        "PRODUCTION_API_URL": "https://www.api.binkslabs.com"
+        "PRODUCTION_API_URL": "https://www.api.binkslabs.com",
+        "GOOGLE_VERIFICATION_ID": "x1RC2zZExKjL4TQ1kkPdQlZECIjkq6C4QB0VURSHlos"
     }
 }
