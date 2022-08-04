592287c2b8b10f72be36780eeab444cbabfb5d9b
binkslabs <pyrotundra@icloud.com>
2022-08-03 23:39:47 -0400

    Add Bing site verification meta tag to the headMetadata.js component.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         "DEV_API_URL": "http://localhost:4000",
         "PRODUCTION_API_URL": "https://www.api.binkslabs.com",
-        "GOOGLE_VERIFICATION_ID": "x1RC2zZExKjL4TQ1kkPdQlZECIjkq6C4QB0VURSHlos"
+        "GOOGLE_VERIFICATION_ID": "x1RC2zZExKjL4TQ1kkPdQlZECIjkq6C4QB0VURSHlos",
+        "BING_VERIFICATION_ID": "3F1A7CFB3D6A9D4EEA4DD323AC7076F4"
     }
 }
